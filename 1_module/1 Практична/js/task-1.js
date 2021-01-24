14900d3b9755b74a49159253972579fa716e455c
unknown <you@example.com>
2021-01-24 16:19:29 +0200

    JS HW 1

---
+++ 1_module/1 Практична/js/task-1.js
@@ -0,0 +1,8 @@
+const string = "Привіт";
+console.log(string);
+
+const number = "123";
+console.log(number);
+
+const isModalOpen = true;
+console.log(isModalOpen);
---
+++ 1_module/1 Практична/js/task-2.js
@@ -0,0 +1,33 @@
+// змінні з let
+
+let userName = "Ira";
+console.log(userName);
+
+let userSurname = "Frankiv";
+console.log(userSurname);
+
+let name;
+console.log(name);
+
+let cat;
+console.log(cat);
+
+let userEmail = "@Ira";
+console.log(userEmail);
+
+// змінні з const
+
+const age = 10;
+console.log(age);
+
+const isAuthenticated = true;
+console.log(isAuthenticated);
+
+const canMerge = false;
+console.log(canMerge);
+
+const guest = null;
+console.log(canMerge);
+
+const yourName = "Vasya";
+console.log(yourName);
